58a8a13efbed2d257ee47223eb067504a1cc87d9
Yahnis Elsts <whiteshadow@w-shadow.com>
2016-01-14 19:32:58 +0200

    Extract the "which plugin is being upgraded?" heuristics to a new class.

--- plugin-update-checker.php
+++ plugin-update-checker.php
@@ -29,10 +29,10 @@ class PluginUpdateChecker_3_0 {
                                //and should be logged to the standard PHP error log.
 	public $scheduler;
 
+	protected $upgraderStatus;
+
 	private $debugBarPlugin = null;
 	private $cachedInstalledVersion = null;
-
-	private $upgradedPluginFile = null; //The plugin that is currently being upgraded by WordPress.
 
 	private $metadataHost = ''; //The host component of $metadataUrl.
 
@@ -72,6 +72,7 @@ class PluginUpdateChecker_3_0 {
 		}
 
 		$this->scheduler = $this->createScheduler($checkPeriod);
+		$this->upgraderStatus = new PucUpgraderStatus_3_0();
 
 		$this->installHooks();
 	}
@@ -578,121 +579,11 @@ class PluginUpdateChecker_3_0 {
 	/**
 	 * Is there and update being installed RIGHT NOW, for this specific plugin?
 	 *
-	 * Caution: This method is unreliable. WordPress doesn't make it easy to figure out what it is upgrading,
-	 * and upgrader implementations are liable to change without notice.
-	 *
 	 * @param WP_Upgrader|null $upgrader The upgrader that's performing the current update.
 	 * @return bool
 	 */
 	public function isPluginBeingUpgraded($upgrader = null) {
-		if ( isset($upgrader) ) {
-			$pluginFile = $this->getPluginBeingUpgradedBy($upgrader);
-			if ( !empty($pluginFile) ) {
-				$this->upgradedPluginFile = $pluginFile;
-			}
-		}
-		return ( !empty($this->upgradedPluginFile) && ($this->upgradedPluginFile === $this->pluginFile) );
-	}
-
-	/**
-	 * Get the file name of the plugin that's currently being upgraded.
-	 *
-	 * @param Plugin_Upgrader|WP_Upgrader $upgrader
-	 * @return string|null
-	 */
-	private function getPluginBeingUpgradedBy($upgrader) {
-		if ( !isset($upgrader, $upgrader->skin) ) {
-			return null;
-		}
-
-		//Figure out which plugin is being upgraded.
-		$pluginFile = null;
-		$skin = $upgrader->skin;
-		if ( $skin instanceof Plugin_Upgrader_Skin ) {
-			if ( isset($skin->plugin) && is_string($skin->plugin) && ($skin->plugin !== '') ) {
-				$pluginFile = $skin->plugin;
-			}
-		} elseif ( isset($skin->plugin_info) && is_array($skin->plugin_info) ) {
-			//This case is tricky because Bulk_Plugin_Upgrader_Skin (etc) doesn't actually store the plugin
-			//filename anywhere. Instead, it has the plugin headers in $plugin_info. So the best we can
-			//do is compare those headers to the headers of installed plugins.
-			$pluginFile = $this->identifyPluginByHeaders($skin->plugin_info);
-		}
-
-		return $pluginFile;
-	}
-
-	/**
-	 * Identify an installed plugin based on its headers.
-	 *
-	 * @param array $searchHeaders The plugin file header to look for.
-	 * @return string|null Plugin basename ("foo/bar.php"), or NULL if we can't identify the plugin.
-	 */
-	private function identifyPluginByHeaders($searchHeaders) {
-		if ( !function_exists('get_plugins') ){
-			/** @noinspection PhpIncludeInspection */
-			require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
-		}
-
-		$installedPlugins = get_plugins();
-		$matches = array();
-		foreach($installedPlugins as $pluginBasename => $headers) {
-			$diff1 = array_diff_assoc($headers, $searchHeaders);
-			$diff2 = array_diff_assoc($searchHeaders, $headers);
-			if ( empty($diff1) && empty($diff2) ) {
-				$matches[] = $pluginBasename;
-			}
-		}
-
-		//It's possible (though very unlikely) that there could be two plugins with identical
-		//headers. In that case, we can't unambiguously identify the plugin that's being upgraded.
-		if ( count($matches) !== 1 ) {
-			return null;
-		}
-
-		return reset($matches);
-	}
-
-	/**
-	 * @access private
-	 *
-	 * @param mixed $input
-	 * @param array $hookExtra
-	 * @return mixed Returns $input unaltered.
-	 */
-	public function setUpgradedPlugin($input, $hookExtra) {
-		if (!empty($hookExtra['plugin']) && is_string($hookExtra['plugin'])) {
-			$this->upgradedPluginFile = $hookExtra['plugin'];
-		} else {
-			$this->upgradedPluginFile = null;
-		}
-		return $input;
-	}
-
-	/**
-	 * @access private
-	 *
-	 * @param array $options
-	 * @return array
-	 */
-	public function setUpgradedPluginFromOptions($options) {
-		if (isset($options['hook_extra']['plugin']) && is_string($options['hook_extra']['plugin'])) {
-			$this->upgradedPluginFile = $options['hook_extra']['plugin'];
-		} else {
-			$this->upgradedPluginFile = null;
-		}
-		return $options;
-	}
-
-	/**
-	 * @access private
-	 *
-	 * @param mixed $input
-	 * @return mixed Returns $input unaltered.
-	 */
-	public function clearUpgradedPlugin($input = null) {
-		$this->upgradedPluginFile = null;
-		return $input;
+		return $this->upgraderStatus->isPluginBeingUpgraded($this->pluginFile, $upgrader);
 	}
 
 	/**
@@ -1200,7 +1091,7 @@ if ( !class_exists('PucScheduler_3_0', false) ):
 
 /**
  * The scheduler decides when and how often to check for updates.
- * It calls @see PluginUpdateChecker_3_0::checkForUpdates() to perform the actual checks.
+ * It calls @see PluginUpdateChecker::checkForUpdates() to perform the actual checks.
  *
  * @version 3.0
  */
@@ -1364,6 +1255,146 @@ class PucScheduler_3_0 {
 }
 
 endif;
+
+/**
+ * A utility class that helps figure out which plugin WordPress is upgrading.
+ *
+ * It may seem strange to have an separate class just for that, but the task is surprisingly complicated.
+ * Core classes like Plugin_Upgrader don't expose the plugin file name during an in-progress update (AFAICT).
+ * This class uses a few workarounds and heuristics to get the file name.
+ */
+class PucUpgraderStatus_3_0 {
+	private $upgradedPluginFile = null; //The plugin that is currently being upgraded by WordPress.
+
+	public function __construct() {
+		//Keep track of which plugin WordPress is currently upgrading.
+		add_filter('upgrader_pre_install', array($this, 'setUpgradedPlugin'), 10, 2);
+		add_filter('upgrader_package_options', array($this, 'setUpgradedPluginFromOptions'), 10, 1);
+		add_filter('upgrader_post_install', array($this, 'clearUpgradedPlugin'), 10, 1);
+		add_action('upgrader_process_complete', array($this, 'clearUpgradedPlugin'), 10, 1);
+	}
+
+	/**
+	 * Is there and update being installed RIGHT NOW, for a specific plugin?
+	 *
+	 * Caution: This method is unreliable. WordPress doesn't make it easy to figure out what it is upgrading,
+	 * and upgrader implementations are liable to change without notice.
+	 *
+	 * @param string $pluginFile The plugin to check.
+	 * @param WP_Upgrader|null $upgrader The upgrader that's performing the current update.
+	 * @return bool True if the plugin identified by $pluginFile is being upgraded.
+	 */
+	public function isPluginBeingUpgraded($pluginFile, $upgrader = null) {
+		if ( isset($upgrader) ) {
+			$upgradedPluginFile = $this->getPluginBeingUpgradedBy($upgrader);
+			if ( !empty($upgradedPluginFile) ) {
+				$this->upgradedPluginFile = $upgradedPluginFile;
+			}
+		}
+		return ( !empty($this->upgradedPluginFile) && ($this->upgradedPluginFile === $pluginFile) );
+	}
+
+	/**
+	 * Get the file name of the plugin that's currently being upgraded.
+	 *
+	 * @param Plugin_Upgrader|WP_Upgrader $upgrader
+	 * @return string|null
+	 */
+	private function getPluginBeingUpgradedBy($upgrader) {
+		if ( !isset($upgrader, $upgrader->skin) ) {
+			return null;
+		}
+
+		//Figure out which plugin is being upgraded.
+		$pluginFile = null;
+		$skin = $upgrader->skin;
+		if ( $skin instanceof Plugin_Upgrader_Skin ) {
+			if ( isset($skin->plugin) && is_string($skin->plugin) && ($skin->plugin !== '') ) {
+				$pluginFile = $skin->plugin;
+			}
+		} elseif ( isset($skin->plugin_info) && is_array($skin->plugin_info) ) {
+			//This case is tricky because Bulk_Plugin_Upgrader_Skin (etc) doesn't actually store the plugin
+			//filename anywhere. Instead, it has the plugin headers in $plugin_info. So the best we can
+			//do is compare those headers to the headers of installed plugins.
+			$pluginFile = $this->identifyPluginByHeaders($skin->plugin_info);
+		}
+
+		return $pluginFile;
+	}
+
+	/**
+	 * Identify an installed plugin based on its headers.
+	 *
+	 * @param array $searchHeaders The plugin file header to look for.
+	 * @return string|null Plugin basename ("foo/bar.php"), or NULL if we can't identify the plugin.
+	 */
+	private function identifyPluginByHeaders($searchHeaders) {
+		if ( !function_exists('get_plugins') ){
+			/** @noinspection PhpIncludeInspection */
+			require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+		}
+
+		$installedPlugins = get_plugins();
+		$matches = array();
+		foreach($installedPlugins as $pluginBasename => $headers) {
+			$diff1 = array_diff_assoc($headers, $searchHeaders);
+			$diff2 = array_diff_assoc($searchHeaders, $headers);
+			if ( empty($diff1) && empty($diff2) ) {
+				$matches[] = $pluginBasename;
+			}
+		}
+
+		//It's possible (though very unlikely) that there could be two plugins with identical
+		//headers. In that case, we can't unambiguously identify the plugin that's being upgraded.
+		if ( count($matches) !== 1 ) {
+			return null;
+		}
+
+		return reset($matches);
+	}
+
+	/**
+	 * @access private
+	 *
+	 * @param mixed $input
+	 * @param array $hookExtra
+	 * @return mixed Returns $input unaltered.
+	 */
+	public function setUpgradedPlugin($input, $hookExtra) {
+		if (!empty($hookExtra['plugin']) && is_string($hookExtra['plugin'])) {
+			$this->upgradedPluginFile = $hookExtra['plugin'];
+		} else {
+			$this->upgradedPluginFile = null;
+		}
+		return $input;
+	}
+
+	/**
+	 * @access private
+	 *
+	 * @param array $options
+	 * @return array
+	 */
+	public function setUpgradedPluginFromOptions($options) {
+		if (isset($options['hook_extra']['plugin']) && is_string($options['hook_extra']['plugin'])) {
+			$this->upgradedPluginFile = $options['hook_extra']['plugin'];
+		} else {
+			$this->upgradedPluginFile = null;
+		}
+		return $options;
+	}
+
+	/**
+	 * @access private
+	 *
+	 * @param mixed $input
+	 * @return mixed Returns $input unaltered.
+	 */
+	public function clearUpgradedPlugin($input = null) {
+		$this->upgradedPluginFile = null;
+		return $input;
+	}
+}
 
 
 if ( !class_exists('PucFactory', false) ):
