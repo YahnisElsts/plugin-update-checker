28f29c940cc22754aff85a99645e3c0a51c097e8
Yahnis Elsts <whiteshadow@w-shadow.com>
2020-02-07 13:07:19 +0200

    Bump version number to 4.9

--- plugin-update-checker.php
+++ plugin-update-checker.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Update Checker Library 4.8
+ * Plugin Update Checker Library 4.9
  * http://w-shadow.com/
  *
- * Copyright 2019 Janis Elsts
+ * Copyright 2020 Janis Elsts
  * Released under the MIT license. See license.txt for details.
  */
 
-require dirname(__FILE__) . '/load-v4p8.php';
+require dirname(__FILE__) . '/load-v4p9.php';
