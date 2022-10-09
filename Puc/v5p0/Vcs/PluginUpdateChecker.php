2b97a8f7124eac3390d157f30d82ea7932e4e8b9
Yahnis Elsts <whiteshadow@w-shadow.com>
2022-10-09 18:46:36 +0300

    Extract common VCS update checker stuff into a new to reduce code duplication.

--- Puc/v5p0/Vcs/PluginUpdateChecker.php
+++ Puc/v5p0/Vcs/PluginUpdateChecker.php
@@ -7,15 +7,7 @@ use YahnisElsts\PluginUpdateChecker\v5p0\Plugin;
 if ( !class_exists(PluginUpdateChecker::class, false) ):
 
 	class PluginUpdateChecker extends Plugin\UpdateChecker implements BaseChecker {
-		/**
-		 * @var string The branch where to look for updates. Defaults to "master".
-		 */
-		protected $branch = 'master';
-
-		/**
-		 * @var Api Repository API client.
-		 */
-		protected $api = null;
+		use VcsCheckerMethods;
 
 		/**
 		 * PluginUpdateChecker constructor.
@@ -262,37 +254,6 @@ if ( !class_exists(PluginUpdateChecker::class, false) ):
 			}
 
 			return $foundAssets;
-		}
-
-		public function setBranch($branch) {
-			$this->branch = $branch;
-			return $this;
-		}
-
-		public function setAuthentication($credentials) {
-			$this->api->setAuthentication($credentials);
-			return $this;
-		}
-
-		public function getVcsApi() {
-			return $this->api;
-		}
-
-		public function getUpdate() {
-			$update = parent::getUpdate();
-
-			if ( isset($update) && !empty($update->download_url) ) {
-				$update->download_url = $this->api->signDownloadUrl($update->download_url);
-			}
-
-			return $update;
-		}
-
-		public function onDisplayConfiguration($panel) {
-			parent::onDisplayConfiguration($panel);
-			$panel->row('Branch', $this->branch);
-			$panel->row('Authentication enabled', $this->api->isAuthenticationEnabled() ? 'Yes' : 'No');
-			$panel->row('API client', get_class($this->api));
 		}
 	}
 
