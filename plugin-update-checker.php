dc93f32388abf0f0a8ea77d72442644d1c82b443
Yahnis Elsts <whiteshadow@w-shadow.com>
2025-05-20 15:20:21 +0300

    Bump version number to 5.6

--- plugin-update-checker.php
+++ plugin-update-checker.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Update Checker Library 5.5
+ * Plugin Update Checker Library 5.6
  * http://w-shadow.com/
  *
  * Copyright 2024 Janis Elsts
  * Released under the MIT license. See license.txt for details.
  */
 
-require dirname(__FILE__) . '/load-v5p5.php';
+require dirname(__FILE__) . '/load-v5p6.php';
